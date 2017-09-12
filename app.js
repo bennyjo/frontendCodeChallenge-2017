0b01aae4b54fef5c31e38ffe003410596bb0332a
Benny Johansson <benny.johansson@reqtest.com>
2017-09-12 20:22:59 +0100

    feat(app): use text component to render collague counts

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-/*global stepper*/
+/* global stepper, text */
+/* exported app */
 
 const app = (function () {
   'use strict';
 
   const stepperElement = document.getElementById('stepper');
+  const addColleagueCounter = document.getElementById('add-collegue-counter');
+  const currentColleageCounter = document.getElementById('current-colleague-counter');
 
   function render() {
     stepper.render(stepperElement, [
@@ -11,6 +14,9 @@ const app = (function () {
       { text: 'Step 2'},
       { text: 'Step 3'},
     ]);
+
+    text.render(addColleagueCounter, 10);
+    text.render(currentColleageCounter, 0);
   }
 
   return { render };
