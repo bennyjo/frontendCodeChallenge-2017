1c470a852521f10ef89f4829f209f12735cf846c
Benny Johansson <benny.johansson@reqtest.com>
2017-09-13 19:52:42 +0100

    feat(UserStore): a web storage based store for user data
    It can:
    1. Add a user
    2. Remove a user
    3. Clear all users
    
    It tells others about what its actions through callbacks:
    1. onAdd
    2. onRemove
    3. onChange
    
    It persists data in desired Web Storage API.

---
+++ userStore/userStore.js
@@ -0,0 +1,76 @@
+/* exported UserStore */
+
+class UserStore {
+  constructor(id, webStore) {
+    if (!id) {
+      throw new Error('UserStore: a store id is required.');
+    }
+    
+    this.id = id;
+    this.webStore = webStore || localStorage;
+
+    const storedUsersString = this.webStore.getItem('id');
+    const users = storedUsersString ? JSON.parse(storedUsersString) : [];
+
+    this.users = users;
+  }
+
+  add(user) {
+    const userStore = this;
+
+    if (!hasUniqueEmail(user)) {
+      throw new Error('UserStore: user email "' + user.email + '" has already been added.');
+    }
+
+    userStore.users.push(user);
+
+    userStore.webStore.setItem(userStore.id + '>' + user.email, JSON.stringify(user));
+
+    userStore.onAdd(user);
+    userStore.onChange();
+
+    function hasUniqueEmail(newUser) {
+      return userStore.users.every(storedUser => storedUser.email !== newUser.email);
+    }
+  }
+
+  remove(userToRemove) {
+    const userStore = this;
+    const userIndex = userStore.users.findIndex(storedUser => storedUser.email === userToRemove.email);
+
+    if (userIndex >= 0) {
+      userStore.users.splice(userIndex, 1);
+      userStore.onRemove(userToRemove);
+      userStore.onChange();
+    }
+
+    userStore.webStore.removeItem(userStore.id + '>' + userToRemove.email);
+  }
+
+  empty() {
+    const userStore = this;
+
+    userStore.users.forEach(user => userStore.webStore.removeItem(userStore.id + '>' + user.email));
+    userStore.users = [];
+
+    userStore.onChange();
+  }
+
+  onAdd(callback) {
+    if (typeof callback === 'function') {
+      callback();
+    }
+  }
+
+  onRemove(callback) {
+    if (typeof callback === 'function') {
+      callback();
+    }
+  }
+
+  onChange(callback) {
+    if (typeof callback === 'function') {
+      callback();
+    }
+  }
+}
---
+++ userStore/userStore.spec.js
@@ -0,0 +1,141 @@
+/* global UserStore */
+
+const { test } = QUnit;
+
+QUnit.module('UserStore', {
+  afterEach: () => { localStorage.clear(); }
+}, () => {
+  test('it requires an id', (assert) => {
+    assert.throws(() => new UserStore());
+  });
+
+  test('it defaults to the localStorage Web Storage API', (assert) => {
+    const userStore = new UserStore('myUserStore');
+
+    assert.strictEqual(userStore.webStore, localStorage);
+  });
+
+  QUnit.module('.add()', () => {
+    test('it adds a user', assert => {
+      const storeId = 'myUserStore';
+      const userStore = new UserStore(storeId, localStorage);
+      const user = { name: 'John Doe', email: 'john.doe@idf.org'};
+
+      userStore.add(user);
+
+      assert.deepEqual(userStore.users[0], user, 'to the users property');
+      assert.deepEqual(JSON.parse(localStorage.getItem(storeId + '>' + user.email)), user, 'to web storage');
+    });
+
+    test('it adds multiple users', assert => {
+      const userStore = new UserStore('myUserStore', localStorage);
+      const user = { name: 'John Doe', email: 'john.doe@idf.org'};
+      const user2 = { name: 'Johanna Doe', email: 'johanna.doe@idf.org'};
+
+      userStore.add(user);
+      userStore.add(user2);
+
+      assert.strictEqual(userStore.users.length, 2);
+    });
+
+    test('it triggers a change', assert => {
+      const userStore = new UserStore('myUserStore', localStorage);
+      const user = { name: 'John Doe', email: 'john.doe@idf.org'};
+      let onChangeCallCount = 0;
+      userStore.onChange(() => onChangeCallCount++);
+
+      userStore.add(user);
+
+      assert.strictEqual(onChangeCallCount, 1);
+    });
+
+    test('it triggers an add change', assert => {
+      const userStore = new UserStore('myUserStore', localStorage);
+      const user = { name: 'John Doe', email: 'john.doe@idf.org'};
+      let onAddCallCount = 0;
+      userStore.onAdd(() => onAddCallCount++);
+
+      userStore.add(user);
+
+      assert.strictEqual(onAddCallCount, 1);
+    });
+
+    test('it only allows users with unique email addresses', assert => {
+      const userStore = new UserStore('myUserStore', localStorage);
+      const user = { name: 'John Doe', email: 'john.doe@idf.org'};
+      const duplicateEmailUser = { name: 'Laura Doe', email: 'john.doe@idf.org'};
+
+      userStore.add(user);
+
+      assert.throws(() => { userStore.add(duplicateEmailUser); });
+    });
+  });
+
+  QUnit.module('.remove()', () => {
+    test('it removes a user', assert => {
+      const storeId = 'myUserStore';
+      const userStore = new UserStore(storeId, localStorage);
+      const user = { name: 'John Doe', email: 'john.doe@idf.org'};
+      userStore.add(user);
+
+      userStore.remove(user);
+
+      assert.strictEqual(userStore.users[0], undefined, 'from the users property');
+      assert.strictEqual(localStorage.getItem(storeId + '>' + user.email), null, 'from web storage');
+    });
+
+    test('it triggers a change', assert => {
+      const userStore = new UserStore('myUserStore', localStorage);
+      const user = { name: 'John Doe', email: 'john.doe@idf.org'};
+      let onChangeCallCount = 0;
+      userStore.add(user);
+      userStore.onChange(() => onChangeCallCount++);
+
+      userStore.remove(user);
+
+      assert.strictEqual(onChangeCallCount, 1);
+    });
+
+    test('it triggers an remove change', assert => {
+      const userStore = new UserStore('myUserStore', localStorage);
+      const user = { name: 'John Doe', email: 'john.doe@idf.org'};
+      let onRemoveCallCount = 0;
+      userStore.add(user);
+      userStore.onRemove(() => onRemoveCallCount++);
+
+      userStore.remove(user);
+
+      assert.strictEqual(onRemoveCallCount, 1);
+    });
+  });
+
+  QUnit.module('.empty()', () => {
+    test('it empties the store', assert => {
+      const storeId = 'myUserStore';
+      const userStore = new UserStore(storeId, localStorage);
+      userStore.add({ name: 'Ivana Doe', email: 'ivana.doe@idf.org'});
+      userStore.add({ name: 'Irvine Doe', email: 'irvine.doe@idf.org'});
+      userStore.add({ name: 'Maya Doe', email: 'maya.doe@idf.org'});
+
+      userStore.empty();
+
+      const webStorageItems = [
+        localStorage.getItem(storeId + '>ivana.doe@idf.org'),
+        localStorage.getItem(storeId + '>irvine.doe@idf.org'),
+        localStorage.getItem(storeId + '>maya.doe@idf.org')
+      ].filter(item => typeof item === 'string');
+      assert.strictEqual(userStore.users.length, 0, 'the users property');
+      assert.strictEqual(webStorageItems.length, 0, 'the web storage');
+    });
+
+    test('it triggers a change', assert => {
+      const userStore = new UserStore('myUserStore', localStorage);
+      let onChangeCallCount = 0;
+      userStore.onChange(() => onChangeCallCount++);
+
+      userStore.empty();
+
+      assert.strictEqual(onChangeCallCount, 1);
+    });
+  });
+});
