209db061a395c768b57d5be71e666ac489d85014
Benny Johansson <benny.johansson@reqtest.com>
2017-09-16 16:43:00 +0100

    feat(app): add newColleagues and a newColleagues form

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* global stepper, text */
+/* global stepper, text, UserStore, UserForm */
 /* exported app */
 
 const app = (function () {
@@ -7,6 +7,16 @@ const app = (function () {
   const stepperElement = document.getElementById('stepper');
   const addColleagueCounter = document.getElementById('add-collegue-counter');
   const currentColleageCounter = document.getElementById('current-colleague-counter');
+
+  const newCollegues = new UserStore('newCollegues');
+  const newColleaguesForm = new UserForm(document.getElementById('newColleaguesForm'));
+
+  // TODO: Instead, run newColleagues.addAnynomous();
+  newCollegues.add({name: 'John Doe', email: 'john.doe@idf.com'});
+
+  newCollegues.onAdd(user => newColleaguesForm.addRow(user));
+  newCollegues.onRemove(user => newColleaguesForm.removeRow(user));
+  newCollegues.onSet((users) => newColleaguesForm.render(users));
 
   function render() {
     stepper.render(stepperElement, [
@@ -17,6 +27,8 @@ const app = (function () {
 
     text.render(addColleagueCounter, 10);
     text.render(currentColleageCounter, 0);
+
+    newColleaguesForm.render(newCollegues.users);
   }
 
   return { render };
