39ca3989e8c38301b5aab779d5f8b8aecd533a50
Benny Johansson <benny.johansson@reqtest.com>
2017-09-20 01:24:52 +0100

    fix(userList): convert NodeList to array
    In order to be able to .find() an object in the array.

--- userList/userList.js
+++ userList/userList.js
@@ -34,7 +34,7 @@ class UserList {
     }
 
     function removeRow(rowId) {
-      const formRows = element.getElementsByClassName('userList__row');
+      const formRows = Array.apply(null, element.getElementsByClassName('userList__row'));
       const rowToRemove = formRows.find(formRow => formRow.getAttribute('data-id') === rowId);
 
       if (rowToRemove) {
